c687c4a5f63aaa336ef3747d980ce2ec5266571d
Joe Martinez <notjoemartinez@tutanota.com>
2024-12-03 17:24:14 -0600

    updated docs

--- README.md
+++ README.md
@@ -5,17 +5,6 @@ A python script to help find chess.com games with "brilliant" moves.
 You need to have a chess.com [premium account](https://www.chess.com/membership?ref_id=74104030) to use this script
 and have your insights generated.  
 
-### Manual review is still required
-
-Unfortunately, the chess.com API does not provide a way to directly
-fetch games with brilliant moves. It does however provide the dates
-of when the brilliant moves were played. This script is a workaround 
-which fetches all the games played by a user on the given dates 
-where a brilliant move was played. While it still requires you to 
-manually check the games in the browser, it will drastically reduce
-the amount of games you need to check and it's guaranteed to include
-all the games with brilliant moves in the output csv. 
-
 
 Usage:
 
@@ -43,3 +32,30 @@ brilliant,url,date,openent_username,user_rating,opponent_rating,result,user_accu
 
 The `brilliant` column is left empty for you to quickly check the url of 
 the game in the browser and mark it as brilliant or not. 
+
+
+### Manual review (suggested)
+
+Unfortunately, the chess.com API does not provide a way to directly
+fetch games with brilliant moves. It does however provide the dates
+of when the brilliant moves were played. This script is a workaround 
+which fetches all the games played by a user on the given dates 
+where a brilliant move was played. While it still requires you to 
+manually check the games in the browser, it will drastically reduce
+the amount of games you need to check and it's guaranteed to include
+all the games with brilliant moves in the output csv. 
+
+
+### Automated Review (use at your own risk)
+
+The `browser_automation.py` script will automate the process of 
+checking the csv outputed by `brilliant_move_finder.py` however 
+it requires you have the FireFox browser installed and runs the 
+risk of your account being banned for botting. If you don't want 
+to risk that I recomend using a burner account. 
+
+Usage: 
+
+Create a new `.env` file like the one in `.env_example` with the 
+path to your default firefox profile. You can find this directory
+by going to `about:profiles` in the firefox url bar. 
--- browser_automation.py
+++ browser_automation.py
@@ -7,7 +7,6 @@ from bs4 import BeautifulSoup
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
 from dotenv import load_dotenv
 
 load_dotenv('.env')
@@ -22,8 +21,6 @@ def main():
     csv_path = sys.argv[1] 
     driver = get_firefox_with_profile()
 
-    # let the user login 
-
     driver.get("https://www.chess.com/login_and_go?returnUrl=https://www.chess.com/")
 
     input("Press Enter to continue...")
@@ -34,20 +31,11 @@ def main():
 
         for row in csv_reader:
             url = row[1]
-            print(f"Visiting {url}")
             driver.get(url)
-
-            # wait for the page to load
             time.sleep(5)
-            # get the page source
-
             page_source = driver.page_source
-
-            # parse the page source
-
             soup = BeautifulSoup(page_source, 'html.parser')
 
-            # find elments with class 'analysis-brilliant'
             brilliants = soup.find_all(class_='analysis-brilliant')
             brilliant_count = 0
             for brilliant in brilliants:
@@ -60,25 +48,11 @@ def main():
                     csv_writer.writerow(row)
                 
 
-
-
-def get_firefox_with_profile(profile_name='default-release'):
-    """
-    Initialize Firefox WebDriver with an existing profile.
-    
-    Args:
-        profile_name (str): Name of the Firefox profile to use (default is 'default-release')
-                          You can find your profile names by visiting 'about:profiles' in Firefox
-    
-    Returns:
-        WebDriver: Selenium WebDriver instance with the specified profile
-    """
+def get_firefox_with_profile():
     options = Options()
    
-    profile_dir = FIREFOX_PROFILE_PATH 
-    
     # Set the profile directory in Firefox options
-    options.set_preference('profile', profile_dir)
+    options.set_preference('profile', FIREFOX_PROFILE_PATH)
     
     # Prevent Firefox from creating a new profile
     options.set_preference("browser.cache.disk.enable", True)
@@ -90,6 +64,6 @@ def get_firefox_with_profile(profile_name='default-release'):
     driver = webdriver.Firefox(options=options)
     return driver
 
-# Example usage
+
 if __name__ == "__main__":
     main()
